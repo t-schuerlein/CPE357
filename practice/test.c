7535cd267265f9925286fbabbcdd6a47a445b50b
crowdedpoem <tasschuerlein@yahoo.com>
2022-01-06 12:04:31 -0800

    where i left off on laptop

--- practice/test.c
+++ practice/test.c
@@ -1,4 +1,11 @@
+
 #include <stdio.h>
+
+struct Class{
+    int i;
+    char c;
+};
+
 
 int main(){
     int a;
@@ -8,6 +15,16 @@ int main(){
     for(i = 0; i < 10; i++){
         arr[i] = i *2;
     }
+
+    struct school
+    {
+        /* data */
+        int i;
+        char c;
+    };
+    
+
+    printf("size is %d", sizeof(school) );
 
     printf("%d\n", arr[1]);
     printf("I am alive\n");
